911fc421204941ba52f5c499d26170299401aa76
B-V-Vulkov <vulkov.borislav@gmail.com>
2020-05-26 19:45:42 +0300

    add home index

--- Source/MobileService/Controllers/HomeController.cs
+++ Source/MobileService/Controllers/HomeController.cs
@@ -26,9 +26,9 @@ namespace MobileService.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var result = this.mapper.Map<UserViewModel>(await this.userService.GetUser());
+            //var result = this.mapper.Map<UserViewModel>(await this.userService.GetUser());
 
-            return View(result);
+            return View();
         }
 
         public IActionResult Privacy()
