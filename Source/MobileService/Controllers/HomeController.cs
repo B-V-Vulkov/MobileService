69fec01c54e3c9bc81d83febcf15065ed4d82a85
B-V-Vulkov <vulkov.borislav@gmail.com>
2020-05-27 18:19:28 +0300

    add Services page

--- Source/MobileService/Controllers/HomeController.cs
+++ Source/MobileService/Controllers/HomeController.cs
@@ -36,6 +36,11 @@ namespace MobileService.Controllers
             return View();
         }
 
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
